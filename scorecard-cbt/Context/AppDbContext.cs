ec5b2e258e19f34d8f0e96af5a305ef19cb14479
adesopequazeem@gmail.com <adesopequazeem@gmail.com>
2022-06-15 09:45:42 +0100

    database was set up

---
+++ scorecard-cbt/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using scorecard_cbt.Models;
+
+namespace scorecard_cbt.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Exam> Exams { get; set; }
+    }
+}
---
+++ scorecard-cbt/Models/Exam.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace scorecard_cbt.Models
+{
+    public class Exam
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string ExamName { get; set; }
+        [Required]
+        public string Duration { get; set; }
+    }
+}
